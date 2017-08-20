dbcdd5737558962a2343d3a23bc05fe6e8cc9742
Philippe Rivière <fil@rezo.net>
2017-08-20 22:24:42 +0200

    contaning

--- gister.sh
+++ gister.sh
@@ -60,7 +60,7 @@ clone the gist repo and put the gistid to clipborad.
 `gister` will pass all arguments to gist as `gist -c -o -d description ...`, so you can use other options that gist understands,
 e.g. `gister description -P` will work.
 
-`gister check` reports all dirty (contaning uncommited changes) gist repositories.
+`gister check` reports all dirty (containing uncommited changes) gist repositories.
 
 Within the root directory of a git repository,
 `gister import id dir branch_name` imports a gist (id) into a subdirectory (dir).
