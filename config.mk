2e5bf6c09a66d134d952caa6875516b0a0e26e91
Jang Rush <weakish@gmail.com>
2022-08-01 01:19:17 +0000

    :memo: Makefile PREFIX config example

--- config.mk
+++ config.mk
@@ -1,3 +1,5 @@
 # Customize below to fit your system
 
 PREFIX = /usr/local
+# PREFIX = "$HOME/.local"
+# Use `/usr/local` as the default value because `$HOME/.local` is not preconfigured on some operating systems.
